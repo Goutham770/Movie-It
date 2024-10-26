2e3a02a4e7055eacbe442e363242e13e96103203
Goutham S <goutham.ms.gk.8@gmail.com>
2024-10-26 13:16:41 +0530

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = ""; 
+const API_KEY = "3416c88b"; 
 const BASE_URL = "https://www.omdbapi.com/";
 
 export const fetchMovies = async (searchTerm, page = 1, type = "") => {
